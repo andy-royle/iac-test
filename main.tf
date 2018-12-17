41a1e53c5002ef9b2b011be6751aac05eadaef07
Andy Royle <andy.royle@halfords.co.uk>
2018-12-17 15:12:59 +0000

    expanding security rule

--- main.tf
+++ main.tf
@@ -81,6 +81,18 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         destination_address_prefix = "*"
     }
 	
+	security_rule {
+        name                       = "SecureRemote"
+        priority                   = 1004
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "85.115.54.202/32"
+        destination_address_prefix = "*"
+    }
+	
     tags {
         environment = "jenkins"
     }
